58b8cd890ff3d988162400d84749153fbe79ee01
osman <osmanustun@outlook.com>
2025-01-14 03:40:25 +0300

    Yetkilendirme tamalandı api ve client olarak ayrı ayrı Öğretmen uygulaması için temel tasarıma başlandı api için get ve post methoları yazılmaya başlandı

---
+++ Saturn.Core.DataAccess/Concrete/SaturnDbContext.cs
@@ -0,0 +1,36 @@
+﻿
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Saturn.Core.Entity.DatabaseEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Saturn.Core.DataAccess.Concrete
+{
+    public class SaturnDbContext:IdentityDbContext<User,UserRole,string>
+    {
+       
+        public SaturnDbContext(DbContextOptions<SaturnDbContext> options) : base(options)
+        {
+            Database.Migrate();
+        }
+        //static readonly string connectionString = "Server = localhost;user ID= silivribilsem_com_saturn; Password =X2kkPrV10kwqfs#e7;Database=silivribilsem_com_saturndb;";// For product
+
+        // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        // {
+        //     //optionsBuilder.UseSqlite("Data Source=C:\\Users\\" + Environment.UserName + "\\SaturnDatabase.db");
+        //     optionsBuilder.UseMySQL(connectionString);
+        // }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Lesson> Groups { get; set; }
+        public DbSet<AttendanceRaw> AttendanceRaws { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
+        //public DbSet<Teacher> Teachers { get; set; }
+
+      
+    }
+}
